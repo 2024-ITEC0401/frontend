e62d31663372b651730c76e6fe6540e92dc4cdf9
hyunaeri <tjgus9650@gmail.com>
2024-09-22 18:17:21 +0900

    Env: 프로젝트 전역에 reset.css 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 
 import { Router } from "./Routes";
+import reset from "./styles/reset";
+import { Global } from "@emotion/react";
 
 export default function App() {
     return (
         <>
+            <Global styles={reset} />
             <BrowserRouter>
                 <Router />
             </BrowserRouter>
