6e7822f420c237cfaf8522a4e1d03205955a956f
toothlessdev <toothless042@gmail.com>
2024-10-06 02:09:37 +0900

    chore: CSS SizeProp 타입 추가

---
+++ src/styles/index.d.ts
@@ -0,0 +1,22 @@
+type CSSAbsoluteUnit = "cm" | "mm" | "Q" | "in" | "pc" | "pt" | "px";
+type CSSRelativeUnit =
+    | "em"
+    | "ex"
+    | "ch"
+    | "rem"
+    | "lh"
+    | "rlh"
+    | "vw"
+    | "vh"
+    | "vmin"
+    | "vmax"
+    | "vb"
+    | "vi"
+    | "svw"
+    | "svh"
+    | "lvw"
+    | "lvh"
+    | "dvw"
+    | "dvh";
+
+type SizeProp = `${number}${CSSAbsoluteUnit | CSSRelativeUnit}`;
