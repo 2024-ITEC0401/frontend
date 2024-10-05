bbad008b6d808a08cc3e9b1dcd3abbce42ee465e
hyunaeri <tjgus9650@gmail.com>
2024-10-06 01:05:35 +0900

    Feat: mockServiceWorker 및 MSW 설정 완료

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,17 @@ import ReactDOM from "react-dom/client";
 
 import App from "@/App";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-);
+async function deferRender() {
+    if (import.meta.env.VITE_RUN_MSW === 'true') {
+        const { worker } = await import('@/__mocks__/Browser');
+        await worker.start();
+    }
+}
+
+deferRender().then(() => {
+    ReactDOM.createRoot(document.getElementById('root')!).render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>,
+    );
+});
