66eeaab0668f394376f3be2a350c7b9c3e6bd324
hyunaeri <tjgus9650@gmail.com>
2024-10-12 22:25:59 +0900

    Feat: Navbar 폴더 구조 추가

---
+++ src/components/navigation/Navbar/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import * as Styles from "@/components/navigation/Navbar/index.style";
+
+const Navbar = () => {
+    return <div>Navbar</div>;
+};
+
+export default Navbar;
