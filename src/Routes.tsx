0c0d9b215dfa6ea0c0f761d88be21da95b6d724c
chominju <cmj021002@knu.ac.kr>
2024-11-04 05:52:19 +0900

    feat: RecommendForProductPage 라우터 설정 추가

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 
 import { ProductRecommendPage } from "@/pages/Recommend/ProductRecommendPage";
+import { RecommendForProductPage } from "@/pages/Recommend/RecommendForProductPage";
 import RecommendCodiPage from "@/pages/RecommendCodiPage";
 import MyClosetPage from "@/pages/myCloset";
 
@@ -13,6 +14,7 @@ export const Router = () => {
                 <Route path="/my-closet" element={<MyClosetPage />}></Route>
                 <Route path="recommend/codi" element={<RecommendCodiPage />} />
                 <Route path="/recommend/product" element={<ProductRecommendPage />} />
+                <Route path="recommend/forproduct" element={<RecommendForProductPage />} />
             </Route>
         </Routes>
     );
