f6ea7fe4b3313f26e5a87e4573fc090465871b53
hyunaeri <tjgus9650@gmail.com>
2024-11-06 15:35:34 +0900

    Feat: 마이 페이지 라우터 설정 추가

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import RecommendCodiPage from "@/pages/RecommendCodiPage";
 import { RecommendProductPage } from "@/pages/RecommendProductPage";
 import MyClosetPage from "@/pages/myCloset";
+import MyPage from "@/pages/myPage";
 
 import { RootLayout } from "@/layouts/RootLayout";
 
@@ -13,6 +14,7 @@ export const Router = () => {
                 <Route path="/my-closet" element={<MyClosetPage />}></Route>
                 <Route path="/recommend/codi" element={<RecommendCodiPage />} />
                 <Route path="/recommend/product" element={<RecommendProductPage />} />
+                <Route path="/my-page" element={<MyPage />} />
             </Route>
         </Routes>
     );
