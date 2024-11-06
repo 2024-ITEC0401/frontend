d08b19a7f5a5456db27a7920c715b0f339aa91da
hyunaeri <tjgus9650@gmail.com>
2024-11-06 18:24:08 +0900

    Feat: MSW 서버 설정 및 핸들러 등록

--- src/__mocks__/Browser.ts
+++ src/__mocks__/Browser.ts
@@ -1,4 +1,5 @@
 import { setupWorker } from 'msw/browser';
 import { clothesMockHandlers } from '@/pages/myCloset/api/clothes.mock';
+import { codiMockHandlers } from '@/pages/RecommendCodiPage/api/codi.mock';
 
-export const worker = setupWorker(...clothesMockHandlers);
+export const worker = setupWorker(...clothesMockHandlers, ...codiMockHandlers);
