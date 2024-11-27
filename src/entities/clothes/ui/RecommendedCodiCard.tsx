41cdeb549ef0526619ea240979e6296e4efd050e
toothlessdev <toothless042@gmail.com>
2024-11-27 22:33:17 +0900

    feature: implements RecommendedCodiCard component

---
+++ src/entities/clothes/ui/RecommendedCodiCard.tsx
@@ -0,0 +1,29 @@
+import HashTag from "@/entities/tag/ui/HashTag";
+
+import { Card } from "@/shared/ui/card";
+
+export interface RecommendedCodiCardProps {
+    imgSrc: string;
+    title: string;
+    hashTags: string[];
+    description: string;
+    createdAt: string;
+}
+
+export const RecommendedCodiCard = ({ imgSrc, title, hashTags, description, createdAt }: RecommendedCodiCardProps) => {
+    return (
+        <Card className="w-full max-w-[820px] h-[200px] p-4 flex">
+            <img src={imgSrc} alt="" className="h-full mr-4 rounded-md aspect-square" />
+            <div className="flex flex-col justify-center gap-1">
+                <h1 className="text-xl font-bold">{title}</h1>
+                <p className="text-gray-500">{createdAt}</p>
+                <p className="line-clamp-2">{description}</p>
+                <div className="flex gap-2 overflow-hidden">
+                    {hashTags.map((hashTag, key) => {
+                        return <HashTag key={key}>{hashTag}</HashTag>;
+                    })}
+                </div>
+            </div>
+        </Card>
+    );
+};
---
+++ src/entities/clothes/ui/__stories__/RecommendedCodiCard.story.ts
@@ -0,0 +1,31 @@
+import { RecommendedCodiCard } from "@/entities/clothes/ui/RecommendedCodiCard";
+
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta: Meta<typeof RecommendedCodiCard> = {
+    title: "RecommendedCodiCard",
+    component: RecommendedCodiCard,
+};
+
+export default meta;
+type Story = StoryObj<typeof RecommendedCodiCard>;
+
+export const Default: Story = {
+    args: {
+        imgSrc: "https://via.placeholder.com/240",
+        title: "코디 제목",
+        createdAt: "2021-10-10",
+        hashTags: [
+            "해시태그1",
+            "해시태그2",
+            "해시태그3",
+            "해시태그4",
+            "해시태그1",
+            "해시태그2",
+            "해시태그3",
+            "해시태그4",
+        ],
+        description:
+            "무더운 여름 에도 깔끔하고 시원하게 입을 수 있는 코디 입니다. 시원한 린넨 바지와 상의와 신발의 포인트 색상을 맞춘 코디 입니다. 무더운 여름 에도 깔끔하고 시원하게 입을 수 있는 코디 입니다. 시원한 린넨 바지와 상의와 신발의 포인트 색상을 맞춘 코디 입니다",
+    },
+};
