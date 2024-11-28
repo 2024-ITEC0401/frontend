4f9d9d48ee7a9acf021735348d0c3e0d5f986344
toothlessdev <toothless042@gmail.com>
2024-11-28 21:03:55 +0900

    style: fix nav bar height

--- src/app/layouts/NavTop.tsx
+++ src/app/layouts/NavTop.tsx
@@ -5,12 +5,12 @@ import { navTop } from "@/app/constants/navTop";
 export const NavTop = () => {
     return (
         <nav className="sticky top-0 w-full mx-auto bg-white shadow-lg h-nav">
-            <div className="flex max-w-[1200px] h-full mx-auto justify-between">
+            <div className="flex max-w-[1200px] h-full mx-auto justify-between px-8">
                 <h1 className="block h-full py-5 font-extrabold text-green-600">LOOK4ME</h1>
-                <ul className="flex items-center h-full gap-4">
+                <ul className="flex gap-4 h-nav">
                     {navTop.map((nav, key) => {
                         return (
-                            <li key={key}>
+                            <li key={key} className="flex items-center h-nav">
                                 <Link to={nav.to} className="text-sm font-semibold">
                                     {nav.label}
                                 </Link>
