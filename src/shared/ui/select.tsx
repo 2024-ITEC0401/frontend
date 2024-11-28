45b2548707c9de8624d7dbee744cd5ba3dd10e24
toothlessdev <toothless042@gmail.com>
2024-11-28 21:07:06 +0900

    feature: add Selector component

--- src/shared/ui/select.tsx
+++ src/shared/ui/select.tsx
@@ -3,6 +3,7 @@ import * as React from "react";
 import { Check, ChevronDown, ChevronUp } from "lucide-react";
 
 import { cn } from "@/shared/lib/utils";
+
 import * as SelectPrimitive from "@radix-ui/react-select";
 
 const Select = SelectPrimitive.Root;
@@ -25,7 +26,7 @@ const SelectTrigger = React.forwardRef<
     >
         {children}
         <SelectPrimitive.Icon asChild>
-            <ChevronDown className="h-4 w-4 opacity-50" />
+            <ChevronDown className="w-4 h-4 opacity-50" />
         </SelectPrimitive.Icon>
     </SelectPrimitive.Trigger>
 ));
@@ -40,7 +41,7 @@ const SelectScrollUpButton = React.forwardRef<
         className={cn("flex cursor-default items-center justify-center py-1", className)}
         {...props}
     >
-        <ChevronUp className="h-4 w-4" />
+        <ChevronUp className="w-4 h-4" />
     </SelectPrimitive.ScrollUpButton>
 ));
 SelectScrollUpButton.displayName = SelectPrimitive.ScrollUpButton.displayName;
@@ -54,7 +55,7 @@ const SelectScrollDownButton = React.forwardRef<
         className={cn("flex cursor-default items-center justify-center py-1", className)}
         {...props}
     >
-        <ChevronDown className="h-4 w-4" />
+        <ChevronDown className="w-4 h-4" />
     </SelectPrimitive.ScrollDownButton>
 ));
 SelectScrollDownButton.displayName = SelectPrimitive.ScrollDownButton.displayName;
@@ -113,7 +114,7 @@ const SelectItem = React.forwardRef<
     >
         <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
             <SelectPrimitive.ItemIndicator>
-                <Check className="h-4 w-4" />
+                <Check className="w-4 h-4" />
             </SelectPrimitive.ItemIndicator>
         </span>
 
@@ -130,6 +131,23 @@ const SelectSeparator = React.forwardRef<
 ));
 SelectSeparator.displayName = SelectPrimitive.Separator.displayName;
 
+type SelectorProps = {
+    className?: string;
+    placeholder?: string;
+    children?: React.ReactNode[];
+} & SelectPrimitive.SelectProps;
+
+const Selector = ({ className, placeholder, children, ...props }: SelectorProps) => {
+    return (
+        <Select {...props}>
+            <SelectTrigger className={className}>
+                <SelectValue placeholder={placeholder} />
+            </SelectTrigger>
+            <SelectContent>{children}</SelectContent>
+        </Select>
+    );
+};
+
 export {
     Select,
     SelectGroup,
@@ -141,4 +159,5 @@ export {
     SelectSeparator,
     SelectScrollUpButton,
     SelectScrollDownButton,
+    Selector,
 };
