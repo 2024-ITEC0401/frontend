634193623521e10ae31a5a0003dc02c78cd94a28
toothlessdev <toothless042@gmail.com>
2024-11-29 00:51:40 +0900

    refactor: add active props to hashtag

--- src/entities/tag/ui/HashTag.tsx
+++ src/entities/tag/ui/HashTag.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 
+import { cn } from "@/shared/lib/utils";
+
 export interface HashTagProps {
+    className?: string;
     children?: React.ReactNode;
+    onClick?: React.MouseEventHandler<HTMLDivElement>;
+    active?: boolean;
 }
 
-export default function HashTag({ children }: HashTagProps) {
+export default function HashTag({ children, className, onClick, active = false }: HashTagProps) {
     return (
-        <div className="flex-shrink-0 inline-flex p-1.5 px-2 bg-[#EDFFEF] rounded-md w-fit h-fit text-primary">
+        <div
+            className={cn(
+                "flex-shrink-0 inline-flex p-1.5 px-2 bg-[#EDFFEF] rounded-md w-fit h-fit text-primary",
+                active ? "bg-primary text-[#EDFFEF]" : "",
+                className,
+            )}
+            onClick={onClick}
+        >
             #{children}
         </div>
     );
