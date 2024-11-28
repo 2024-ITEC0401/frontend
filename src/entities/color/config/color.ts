6b0d82a3f1d83b78dc093f409b8ccc0760560687
toothlessdev <toothless042@gmail.com>
2024-11-28 21:06:07 +0900

    chore: add color, styles constants

---
+++ src/entities/color/config/color.ts
@@ -0,0 +1,132 @@
+export const colors = [
+    {
+        colorLabel: "흰색",
+        colorValue: "WHITE",
+        color: "#ffffff",
+    },
+    {
+        colorLabel: "크림",
+        colorValue: "CREAM",
+        color: "#fffdd0",
+    },
+    {
+        colorLabel: "베이지",
+        colorValue: "BEIGE",
+        color: "#f5f5dc",
+    },
+    {
+        colorLabel: "연회색",
+        colorValue: "LIGHT_GRAY",
+        color: "#d3d3d3",
+    },
+    {
+        colorLabel: "진회색",
+        colorValue: "DARK_GRAY",
+        color: "#a9a9a9",
+    },
+    {
+        colorLabel: "검정",
+        colorValue: "BLACK",
+        color: "#000000",
+    },
+    {
+        colorLabel: "연분홍",
+        colorValue: "LIGHT_PINK",
+        color: "#ffb6c1",
+    },
+    {
+        colorLabel: "노랑",
+        colorValue: "YELLOW",
+        color: "#ffff00",
+    },
+    {
+        colorLabel: "연두",
+        colorValue: "LIGHT_GREEN",
+        color: "#90ee90",
+    },
+    {
+        colorLabel: "민트",
+        colorValue: "MINT",
+        color: "#3eb489",
+    },
+    {
+        colorLabel: "하늘색",
+        colorValue: "SKY_BLUE",
+        color: "#87ceeb",
+    },
+    {
+        colorLabel: "연보라",
+        colorValue: "LIGHT_PURPLE",
+        color: "#e6e6fa",
+    },
+    {
+        colorLabel: "분홍",
+        colorValue: "PINK",
+        color: "#ffc0cb",
+    },
+    {
+        colorLabel: "코랄",
+        colorValue: "CORAL",
+        color: "#ff7f50",
+    },
+    {
+        colorLabel: "주황",
+        colorValue: "ORANGE",
+        color: "#ffa500",
+    },
+    {
+        colorLabel: "초록",
+        colorValue: "GREEN",
+        color: "#008000",
+    },
+    {
+        colorLabel: "파랑",
+        colorValue: "BLUE",
+        color: "#0000ff",
+    },
+    {
+        colorLabel: "보라",
+        colorValue: "PURPLE",
+        color: "#800080",
+    },
+    {
+        colorLabel: "빨강",
+        colorValue: "RED",
+        color: "#ff0000",
+    },
+    {
+        colorLabel: "카멜",
+        colorValue: "CAMEL",
+        color: "#c19a6b",
+    },
+    {
+        colorLabel: "갈색",
+        colorValue: "BROWN",
+        color: "#a52a2a",
+    },
+    {
+        colorLabel: "카키",
+        colorValue: "KHAKI",
+        color: "#f0e68c",
+    },
+    {
+        colorLabel: "네이비",
+        colorValue: "NAVY",
+        color: "#000080",
+    },
+    {
+        colorLabel: "와인",
+        colorValue: "WINE",
+        color: "#722f37",
+    },
+    {
+        colorLabel: "금색",
+        colorValue: "GOLD",
+        color: "#ffd700",
+    },
+    {
+        colorLabel: "은색",
+        colorValue: "SILVER",
+        color: "#c0c0c0",
+    },
+];
---
+++ src/entities/style/config/styles.ts
@@ -0,0 +1,57 @@
+export const styles = [
+    {
+        krLabel: "데일리",
+        enLabel: "Daily",
+        value: "DAILY",
+    },
+    {
+        krLabel: "직장",
+        enLabel: "Work",
+        value: "WORK",
+    },
+    {
+        krLabel: "데이트",
+        enLabel: "Date",
+        value: "DATE",
+    },
+    {
+        krLabel: "의식",
+        enLabel: "Ceremony",
+        value: "CEREMONY",
+    },
+    {
+        krLabel: "여행",
+        enLabel: "Travel",
+        value: "TRAVEL",
+    },
+    {
+        krLabel: "홈웨어",
+        enLabel: "Homewear",
+        value: "HOMEWEAR",
+    },
+    {
+        krLabel: "파티",
+        enLabel: "Party",
+        value: "PARTY",
+    },
+    {
+        krLabel: "운동",
+        enLabel: "Exercise",
+        value: "EXERCISE",
+    },
+    {
+        krLabel: "특별한 날",
+        enLabel: "Special Day",
+        value: "SPECIAL_DAY",
+    },
+    {
+        krLabel: "학교",
+        enLabel: "School",
+        value: "SCHOOL",
+    },
+    {
+        krLabel: "기타",
+        enLabel: "Other",
+        value: "OTHER",
+    },
+];
