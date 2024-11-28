5f7506dde257283cc8c9970cfb4f76f263e2c218
toothlessdev <toothless042@gmail.com>
2024-11-28 21:08:31 +0900

    chore: add scafolding for each pages

--- src/app/Routes.tsx
+++ src/app/Routes.tsx
@@ -5,18 +5,19 @@ import { RootLayout } from "@/app/layouts/RootLayout";
 
 import SignInPage from "@/pages/auth/SignInPage";
 import HomePage from "@/pages/home/HomePage";
+import ProfilePage from "@/pages/profile/ProfilePage";
 
 export const Router = () => {
     return (
         <Routes>
             <Route path="/" element={<RootLayout />}>
                 <Route path="/" element={<HomePage />} />
-                {/* <Route path="/" element={<HomePage />} />
-                <Route path="/my-closet" element={<MyClosetPage />}></Route>
+                <Route path="/profile" element={<ProfilePage />} />
+                {/* <Route path="/my-closet" element={<MyClosetPage />}></Route>
                 <Route path="/recommend/codi" element={<RecommendCodiPage />} />
                 <Route path="/recommend/product" element={<RecommendProductPage />} />
                 <Route path="/recommend/forproduct" element={<RecommendForProductPage />} />
-                <Route path="/my-page" element={<MyPage />} /> */}
+                <Route path="/my-page" element={<MyPage />} />  */}
             </Route>
 
             <Route path="/auth" element={<AuthLayout />}>
