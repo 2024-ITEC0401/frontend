50a220ec57cfc367d5218fad308c2a241b1dc636
toothlessdev <toothless042@gmail.com>
2024-11-29 09:45:54 +0900

    chore: add category, season constants

---
+++ src/entities/clothes/config/season.ts
@@ -0,0 +1,18 @@
+export const seasons = [
+    {
+        seasonLabel: "봄",
+        seasonValue: "SPRING",
+    },
+    {
+        seasonLabel: "여름",
+        seasonValue: "SUMMER",
+    },
+    {
+        seasonLabel: "가을",
+        seasonValue: "AUTUMN",
+    },
+    {
+        seasonLabel: "겨울",
+        seasonValue: "WINTER",
+    },
+];
