0f2d86b8f1375bb9b94c6a5ed9e489a7f6847d02
hyunaeri <tjgus9650@gmail.com>
2024-11-29 17:21:30 +0900

    Feat: 회원가입 페이지 Navigate 추가

--- src/pages/auth/SignInPage.tsx
+++ src/pages/auth/SignInPage.tsx
@@ -37,8 +37,8 @@ export default function SignInPage() {
     return (
         <div>
             <div className="mb-10 text-center">
-                <h1 className="text-3xl font-extrabold ">LOOK4ME에 오신 것을 환영합니다</h1>
-                <p className="my-1 text-gray-500">로그인 후 맞춤화된 코디 추천 서비스를 경험해보세요</p>
+                <h1 className="text-3xl font-extrabold ">LOOK4ME 에 오신 것을 환영합니다!</h1>
+                <p className="my-1 text-gray-500">로그인 후 맞춤화된 코디 추천 서비스를 경험해보세요.</p>
             </div>
 
             <div className="flex flex-col gap-4">
@@ -71,8 +71,13 @@ export default function SignInPage() {
                 </Button>
 
                 <p className="text-center">
-                    <span>아직 LOOK4ME 회원이 아니신가요?</span>
-                    <span className="font-semibold text-primary"> 회원가입</span>
+                    <span>아직 LOOK4ME 회원이 아니신가요? </span>
+                    <span
+                        className="font-semibold text-primary ml-1 cursor-pointer"
+                        onClick={() => navigate("/auth/signup")}
+                    >
+                        회원가입
+                    </span>
                 </p>
             </div>
         </div>
