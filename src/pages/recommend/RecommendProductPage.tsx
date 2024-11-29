73d9ec82b5843b8fd83d3d31a73a4a62b1d2e6c6
toothlessdev <toothless042@gmail.com>
2024-11-29 14:41:10 +0900

    feature: recommend product page

---
+++ src/pages/recommend/RecommendProductPage.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+import { FilterCloset } from "@/features/home/ui/FilterCloset";
+
+import { RecommendedProductCard } from "@/entities/clothes/ui/RecommendedProductCard";
+
+export default function RecommendProductPage() {
+    const [filter, setFilter] = useState<Record<string, string>>({});
+
+    return (
+        <div>
+            <div className="sticky top-0 flex items-center justify-between py-1 bg-white">
+                <div>
+                    <h1 className="mt-2 text-xl font-bold">맞춤 상품 추천</h1>
+                    <p className="mb-2 text-sm">총 30개</p>
+                </div>
+                <div className="flex gap-1">
+                    <FilterCloset onFilterChange={(filter) => setFilter(filter)} />
+                </div>
+            </div>
+
+            <div className="flex flex-wrap gap-1.5 justify-center my-2">
+                {Array.from({ length: 30 }).map((_, key) => {
+                    return <RecommendedProductCard key={key} title={"흰티"} count={3} />;
+                })}
+            </div>
+        </div>
+    );
+}
