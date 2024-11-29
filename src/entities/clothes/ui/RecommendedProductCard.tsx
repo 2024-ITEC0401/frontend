4214f44abac56b608dbdf2c2abf080952eead9a5
toothlessdev <toothless042@gmail.com>
2024-11-29 14:40:46 +0900

    style: change product card text color

--- src/entities/clothes/ui/RecommendedProductCard.tsx
+++ src/entities/clothes/ui/RecommendedProductCard.tsx
@@ -13,9 +13,9 @@ export const RecommendedProductCard = ({ imgSrc, title, count }: RecommendedProd
             <div className="my-1">
                 <h2 className="text-lg font-bold text-center line-clamp-1">{title}</h2>
 
-                <p className="text-sm text-center line-clamp-1">
+                <p className="text-sm text-center text-gray-500 line-clamp-1">
                     내 옷장에 있는
-                    <span className="font-bold text-primary"> {count}개</span> 옷과 잘 어울림
+                    <span className="font-semibold text-primary"> {count}개</span> 옷과 잘 어울림
                 </p>
             </div>
         </Card>
