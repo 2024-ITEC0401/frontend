996b2a8af8df56498aaeca8ac3e7bd4832cec0ec
hyunaeri <tjgus9650@gmail.com>
2024-11-29 16:10:23 +0900

    Feat: axiosInstance 구현

---
+++ src/app/config/axios.ts
@@ -0,0 +1,51 @@
+import axios from "axios";
+import type { AxiosInstance, AxiosRequestConfig, InternalAxiosRequestConfig } from "axios";
+
+import { BASE_URL } from "@/app/constants/URL";
+
+import { useAuthStore } from "@/entities/tokens/stores/authStore";
+
+import { QueryClient } from "@tanstack/react-query";
+
+const IS_MSW = import.meta.env.VITE_RUN_MSW === "true";
+
+const baseURL = IS_MSW ? "http://localhost:5173" : `${BASE_URL}`;
+
+export const createInstance = (config: AxiosRequestConfig): AxiosInstance => {
+    const instance = axios.create({
+        timeout: 10000,
+        ...config,
+        baseURL,
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            ...config.headers,
+        },
+    });
+
+    instance.interceptors.request.use(
+        (config: InternalAxiosRequestConfig) => {
+            const { accessToken } = useAuthStore.getState();
+            if (accessToken) {
+                config.headers["Authorization"] = `Bearer ${accessToken}`;
+            }
+            return config;
+        },
+        (error: unknown) => Promise.reject(error),
+    );
+
+    return instance;
+};
+
+export const fetchInstance = createInstance({});
+
+export const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            retry: 3,
+            refetchOnMount: true,
+            refetchOnReconnect: true,
+            refetchOnWindowFocus: true,
+        },
+    },
+});
