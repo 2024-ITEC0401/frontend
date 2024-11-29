e475673c477d67db2f7425dba9d43b705b87d7c0
toothlessdev <toothless042@gmail.com>
2024-11-29 20:54:38 +0900

    style: recommend codi card with, description height

--- src/entities/clothes/ui/RecommendedCodiCard.tsx
+++ src/entities/clothes/ui/RecommendedCodiCard.tsx
@@ -12,12 +12,12 @@ export interface RecommendedCodiCardProps {
 
 export const RecommendedCodiCard = ({ imgSrc, title, hashTags, description, createdAt }: RecommendedCodiCardProps) => {
     return (
-        <Card className="w-full max-w-[1200px] h-[200px] p-4 flex">
+        <Card className="w-full max-w-[1400px] h-[200px] p-4 flex">
             <img src={imgSrc} alt="" className="h-full mr-4 rounded-md aspect-square" />
             <div className="flex flex-col justify-center gap-1">
                 <h1 className="text-xl font-bold">{title}</h1>
                 <p className="text-gray-500">{createdAt}</p>
-                <p className="line-clamp-2">{description}</p>
+                <p className="h-12 line-clamp-2">{description}</p>
                 <div className="flex gap-2 overflow-hidden">
                     {hashTags.map((hashTag, key) => {
                         return <HashTag key={key}>{hashTag}</HashTag>;
