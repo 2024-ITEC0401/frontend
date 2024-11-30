d3ef8964725513d6e59486ee9314997a2d95df4b
chominju <cmj021002@knu.ac.kr>
2024-12-01 01:39:45 +0900

    feat: useViewProfile 훅 구현

---
+++ src/features/profile/hooks/useViewProfile.tsx
@@ -0,0 +1,30 @@
+import { fetchInstance } from "@/app/config/axios";
+
+import { useQuery } from "@tanstack/react-query";
+
+interface ViewProfileResponseBody {
+    email: string;
+    nickName: string;
+    gender: number;
+    age: number;
+    height: number;
+    weight: number;
+    tone: string;
+    colorList: string[];
+    styleList: string[];
+}
+
+const viewProfile = async () => {
+    const response = await fetchInstance.get<ViewProfileResponseBody>("/api/v1/user/profile");
+    console.log(response.data);
+    return response.data;
+};
+
+export const useViewProfile = () => {
+    const query = useQuery({
+        queryKey: ["profile"],
+        queryFn: () => viewProfile(),
+    });
+
+    return { ...query };
+};
