9893189ecefd723b2bfe6c0e299f9097adff85e2
hyunaeri <tjgus9650@gmail.com>
2024-12-01 01:18:48 +0900

    Chore: 스프링 서버와 통신하는 인스턴스 일부 수정

--- src/app/config/axios.ts
+++ src/app/config/axios.ts
@@ -7,9 +7,7 @@ import { useAuthStore } from "@/entities/tokens/stores/authStore";
 
 import { QueryClient } from "@tanstack/react-query";
 
-const IS_MSW = import.meta.env.VITE_RUN_MSW === "true";
-
-const baseURL = IS_MSW ? "http://localhost:5173" : `${BASE_URL}`;
+const baseURL = BASE_URL;
 
 export const createInstance = (config: AxiosRequestConfig): AxiosInstance => {
     const instance = axios.create({
