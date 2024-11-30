d9a4127563c2fac81c0e428a3823202044db2899
hyunaeri <tjgus9650@gmail.com>
2024-11-30 23:21:55 +0900

    Feat: 메인페이지 → 유저가 등록한 모든 옷 조회 API 연결

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 
+import { useFetchAllClothes } from "@/features/home/hooks/useFetchAllclothes";
 import { FilterCloset } from "@/features/home/ui/FilterCloset";
 
 import { AddClothModal } from "@/entities/clothes/ui/AddClothModal";
@@ -7,17 +8,26 @@ import { ClothCard } from "@/entities/clothes/ui/ClothCard";
 
 export default function HomePage() {
     const [filter, setFilter] = useState<Record<string, string>>({});
+    const { data: clothes, isLoading, isError } = useFetchAllClothes();
 
     useEffect(() => {
-        console.table(filter);
+        // console.table(filter);
     }, [filter]);
+
+    if (isLoading) {
+        return <div>로딩 중...</div>;
+    }
+
+    if (isError) {
+        return <div>옷 데이터를 가져오는 중에 문제가 발생했습니다.</div>;
+    }
 
     return (
         <div>
             <div className="sticky top-0 flex items-center justify-between py-1 bg-white">
                 <div>
                     <h1 className="mt-2 text-xl font-bold">내 옷장</h1>
-                    <p className="mb-2 text-sm">총 30개</p>
+                    <p className="mb-2 text-sm">총 {clothes?.length || 0}개</p>
                 </div>
 
                 <div className="flex gap-1">
@@ -28,8 +38,8 @@ export default function HomePage() {
             </div>
 
             <div className="flex flex-wrap justify-center gap-1.5 my-2">
-                {Array.from({ length: 40 }).map((_, index) => (
-                    <ClothCard key={index} title={"옷"} description={"옷 정보"} />
+                {clothes?.map((cloth, index) => (
+                    <ClothCard key={index} imgSrc={cloth.imageUri} title={cloth.name} description={cloth.description} />
                 ))}
             </div>
         </div>
