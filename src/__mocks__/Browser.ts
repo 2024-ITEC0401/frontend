f3de74ac7f56203135e50882f21924ee2e370b5f
chominju <cmj021002@knu.ac.kr>
2024-12-06 06:40:35 +0900

    remove: import header

--- src/__mocks__/Browser.ts
+++ src/__mocks__/Browser.ts
@@ -1,6 +1,3 @@
-// import { codiMockHandlers } from "@/pages/RecommendCodiPage/api/codi.mock";
-// import { productMockHandlers } from "@/pages/RecommendProductPage/api/product.mock";
-// import { clothesMockHandlers } from "@/pages/myCloset/api/clothes.mock";
 import { setupWorker } from "msw/browser";
 
 export const worker = setupWorker();
