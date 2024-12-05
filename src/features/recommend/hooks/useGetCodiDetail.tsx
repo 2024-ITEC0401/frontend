3b12e75fff63bf139c9586eaff75595c667df9fd
chominju <cmj021002@knu.ac.kr>
2024-12-06 05:43:25 +0900

    feat: useGetCodiDetail 커스텀 훅 구현

---
+++ src/features/recommend/hooks/useGetCodiDetail.tsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+
+import { fetchInstance } from "@/app/config/axios";
+
+import { useQuery } from "@tanstack/react-query";
+
+interface CodiDetailResponseBody {
+    id: number;
+    name: string;
+    description: string;
+    hashtags: string;
+    createdAt: string;
+    clothingList: [
+        {
+            id: number;
+            imageUri: string;
+            name: string;
+            mainCategory: string;
+            subCategory: string;
+            baseColor: string;
+            pointColor: string;
+            textile: string;
+            pattern: string;
+            season: string;
+            style: string;
+            description: string;
+        },
+    ];
+}
+
+export const getCodiDetail = async (id: number): Promise<CodiDetailResponseBody> => {
+    const response = await fetchInstance.get(`/api/v1/coordination/${id}`);
+    return response.data;
+};
+
+export const useGetCodiDetail = () => {
+    const [id, setId] = useState<number>(0);
+
+    const query = useQuery({
+        queryKey: ["codiDetail", id],
+        queryFn: () => getCodiDetail(id),
+        enabled: !!id,
+    });
+
+    const handleGetCodiDetail = (newId: number) => {
+        setId(newId);
+        query.refetch();
+    };
+
+    return { handleGetCodiDetail, ...query };
+};
