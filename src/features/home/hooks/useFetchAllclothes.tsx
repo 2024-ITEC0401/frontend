f73c043dc77d388ffea214ac3a2c69ea49807953
toothlessdev <toothless042@gmail.com>
2024-12-06 17:11:34 +0900

    feature: implements ClothCardModal

--- src/features/home/hooks/useFetchAllclothes.tsx
+++ src/features/home/hooks/useFetchAllclothes.tsx
@@ -2,7 +2,7 @@ import { fetchInstance } from "@/app/config/axios";
 
 import { useQuery } from "@tanstack/react-query";
 
-interface ClothInfo {
+export interface ClothInfo {
     id: number;
     imageUri: string;
     name: string;
