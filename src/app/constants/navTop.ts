788dfa70e82d9d7a8779622d5dae068762d726a0
toothlessdev <toothless042@gmail.com>
2024-12-06 18:35:49 +0900

    chore: add routes to chat page

--- src/app/constants/navTop.ts
+++ src/app/constants/navTop.ts
@@ -4,12 +4,12 @@ export const navTop = [
         label: "내 옷장 관리",
     },
     {
-        to: "/recommend/product",
-        label: "맞춤 상품 추천",
-    },
-    {
         to: "/recommend/codi",
         label: "코디 추천",
+    },
+    {
+        to: "/recommend/chat",
+        label: "채팅",
     },
     {
         to: "/profile",
