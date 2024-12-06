80a33495d7f6fe89e78540c73d0d2feec581c98c
hyunaeri <tjgus9650@gmail.com>
2024-12-07 00:51:09 +0900

    Fix: setColorList 추가

--- src/pages/profile/ProfilePage.tsx
+++ src/pages/profile/ProfilePage.tsx
@@ -143,6 +143,7 @@ export default function ProfilePage() {
                             if (selectedColor) {
                                 setColorList([selectedColor.colorValue]);
                             }
+                            setColorList([value]);
                         }}
                     />
                 </div>
