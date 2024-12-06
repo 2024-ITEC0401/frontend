60301f59e809c7106446707a6c872eecb9625f9f
hyunaeri <tjgus9650@gmail.com>
2024-12-06 21:03:41 +0900

    Chore: 디버깅용 코드 제거

--- src/entities/clothes/hooks/useAddCloth.tsx
+++ src/entities/clothes/hooks/useAddCloth.tsx
@@ -123,17 +123,13 @@ export const useAddCloth = () => {
 
         uploadCloth(requestData, {
             onSuccess: () => {
-                console.log("옷 등록 성공:", requestData);
                 alert("옷이 성공적으로 등록되었습니다!");
 
                 queryClient.invalidateQueries({ queryKey: ["allClothes"] });
-
                 dispatch({ type: "RESET" });
                 setInvalidFields([]);
             },
-            onError: (error) => {
-                console.log("옷 등록 실패:", requestData);
-                console.error("옷 등록 실패:", error);
+            onError: () => {
                 alert("옷 등록에 실패했습니다. 다시 시도해주세요.");
             },
         });
