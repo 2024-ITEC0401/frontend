a693dcf693a184d3d05d85001d6a8271392e3ff0
chominju <cmj021002@knu.ac.kr>
2024-12-07 22:33:30 +0900

    feat: useChat 구현

--- src/features/recommend/hooks/useChat.tsx
+++ src/features/recommend/hooks/useChat.tsx
@@ -8,16 +8,35 @@ interface ChatRequestBody {
     natural_language: string;
 }
 
-const chat = async (data: ChatRequestBody): Promise<void> => {
-    await fetchInstance.post("/api/v1/coordination/rec/natural-language", data);
+interface ChatResponseBody {
+    id: number;
+    name: string;
+    description: string;
+    hashtags: string;
+    createdAt: string;
+    clothingImages: string[];
+}
+
+const chat = async (data: ChatRequestBody): Promise<ChatResponseBody> => {
+    const response = await fetchInstance.post("/api/v1/coordination/rec/natural-language", data);
+    return response.data;
 };
 
 export const useChat = () => {
     const inputRef = useRef<HTMLInputElement>(null);
-    const [chatHistory, setChatHistory] = useState<string[]>([]);
+    const [chatHistory, setChatHistory] = useState<(string | ChatResponseBody)[]>([]);
+    const [isPending, setIsPending] = useState(false);
 
     const { mutate } = useMutation({
         mutationFn: (data: ChatRequestBody) => chat(data),
+        onMutate: () => {
+            if (!isPending) {
+                setIsPending(true);
+            }
+        },
+        onSuccess: (data) => {
+            setChatHistory((prev) => [...prev, data]);
+        },
     });
 
     const handleSend = useCallback(() => {
@@ -29,5 +48,5 @@ export const useChat = () => {
         }
     }, [mutate]);
 
-    return { inputRef, chatHistory, handleSend };
+    return { inputRef, chatHistory, handleSend, isPending };
 };
