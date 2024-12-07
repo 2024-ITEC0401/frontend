aa4380766988a8404dc438e35a82e7b39ce0dee3
chominju <cmj021002@knu.ac.kr>
2024-12-08 00:06:16 +0900

    feat: enter 누르면  메세지 전송되도록

--- src/features/recommend/ui/ChatInput.tsx
+++ src/features/recommend/ui/ChatInput.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef } from "react";
+import React, { forwardRef, useRef } from "react";
 
 import { Send } from "lucide-react";
 
@@ -10,6 +10,7 @@ export interface ChatInputProps {
 }
 
 export const ChatInput = forwardRef<HTMLInputElement, ChatInputProps>(({ onClick }, ref) => {
+    const buttonRef = useRef<HTMLButtonElement>(null);
     return (
         <section className="fixed bottom-0 w-full max-w-[1180px] h-[50px] bg-white shadow-lg mx-auto px-[10px]">
             <div className="relative w-full mx-auto">
@@ -18,8 +19,13 @@ export const ChatInput = forwardRef<HTMLInputElement, ChatInputProps>(({ onClick
                     type="text"
                     className="h-[40px] rounded-full w-full absolute pr-[50px]"
                     placeholder="내용을 입력해주세요"
+                    onKeyDown={(e) => {
+                        if (e.key === "Enter") {
+                            buttonRef.current?.click();
+                        }
+                    }}
                 />
-                <Button className="rounded-full h-[40px] w-[40px] absolute right-0" onClick={onClick}>
+                <Button ref={buttonRef} className="rounded-full h-[40px] w-[40px] absolute right-0" onClick={onClick}>
                     <Send />
                 </Button>
             </div>
